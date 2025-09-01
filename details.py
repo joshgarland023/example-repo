cb045c10c8f38a7f81ffd5f87897bf457a537606
joshgarland023 <joshgarland023@gmail.com>
2025-09-01 13:10:38 -0500

    Add files via upload

---
+++ details.py
@@ -0,0 +1,14 @@
+1. # Ask the user for their name
+name = input("Enter your full name: ")
+
+# Ask the user for their age
+age = input("Enter your age: ")
+
+# Ask the user for their house number
+house_number = input("Enter your house number: ")
+
+# Ask the user for your street name
+street_name = input("Enter your street name: ")
+
+# Print a single formatted sentence with all details
+print("This is " + name + ". He is " + age + " years old and lives at the house number " + house_number + " on " + street_name + ".")
